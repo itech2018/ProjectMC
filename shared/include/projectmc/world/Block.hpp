e02f9fcc5587b191042caf66e3c18d096dd51f7c
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-26 00:07:36 +0100

    Add block texture face metadata

--- shared/include/projectmc/world/Block.hpp
+++ shared/include/projectmc/world/Block.hpp
@@ -3,13 +3,11 @@
 #include <string>
 #include <string_view>
 namespace projectmc::world {
-using BlockId = std::uint16_t;
-enum class BlockMaterial : std::uint8_t { Air, Soil, Stone, Wood, Foliage, Sand, Water, Metal };
+using BlockId=std::uint16_t;
+enum class BlockMaterial:std::uint8_t{Air,Soil,Stone,Wood,Foliage,Sand,Water,Metal};
+struct BlockTextures { std::string top,side,bottom; };
 struct BlockDefinition {
-  BlockId id{};
-  std::string name;
-  BlockMaterial material{BlockMaterial::Air};
-  bool solid{false};
-  bool transparent{true};
+ BlockId id{};std::string name;BlockMaterial material{BlockMaterial::Air};
+ bool solid{false};bool transparent{true};BlockTextures textures{};
 };
 }
