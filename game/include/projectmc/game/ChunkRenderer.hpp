7f25507305ffb09f36f5b8231d2e1c5e808e0ba3
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-26 00:09:31 +0100

    Pass texture atlas into chunk renderer

--- game/include/projectmc/game/ChunkRenderer.hpp
+++ game/include/projectmc/game/ChunkRenderer.hpp
@@ -3,12 +3,13 @@
 #include "projectmc/world/Chunk.hpp"
 #include "projectmc/world/BlockRegistry.hpp"
 #include "projectmc/game/Camera.hpp"
+#include "projectmc/game/TextureAtlas.hpp"
 namespace projectmc::game {
 class ChunkRenderer {
 public:
- struct Point { float x{}, y{}; bool valid{false}; };
- void render(SDL_Renderer* renderer,const world::Chunk& chunk,const world::BlockRegistry& blocks,const Camera& camera,int viewportWidth,int viewportHeight);
- void renderSelection(SDL_Renderer* renderer,int x,int y,int z,const Camera& camera,int viewportWidth,int viewportHeight);
- [[nodiscard]] Point project(float x,float y,float z,const Camera& camera,int w,int h) const;
+ struct Point{float x{},y{};bool valid{false};};
+ void render(SDL_Renderer*,const world::Chunk&,const world::BlockRegistry&,const TextureAtlas&,const Camera&,int,int);
+ void renderSelection(SDL_Renderer*,int,int,int,const Camera&,int,int);
+ [[nodiscard]] Point project(float,float,float,const Camera&,int,int)const;
 };
 }
