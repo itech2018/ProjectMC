7ac3e63303f803ef5a43605cf13404fa03a0e09a
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-25 23:57:16 +0100

    Connect player controller

--- game/include/projectmc/game/Application.hpp
+++ game/include/projectmc/game/Application.hpp
@@ -4,6 +4,8 @@
 #include "projectmc/game/Input.hpp"
 #include "projectmc/game/Camera.hpp"
 #include "projectmc/game/ChunkRenderer.hpp"
+#include "projectmc/game/Player.hpp"
+#include "projectmc/game/Raycast.hpp"
 #include "projectmc/world/World.hpp"
 namespace projectmc::game {
 class Application {
@@ -14,7 +16,7 @@ public:
 private:
  void processEvents();void update(double dt);void render();void interact(bool place);
  projectmc::GameConfig config_;SDL_Window* window_{nullptr};SDL_Renderer* renderer_{nullptr};
- InputState input_{};Camera camera_{};ChunkRenderer chunkRenderer_{};world::World world_{};
+ InputState input_{};Camera camera_{};Player player_{};ChunkRenderer chunkRenderer_{};world::World world_{};
  bool running_{false};
 };
 }
