2147783fe8fdebc77e9de6b0daeec486d1e7fb05
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-26 18:01:51 +0100

    Draw HUD overlay in GPU frame

--- game/src/Application.cpp
+++ game/src/Application.cpp
@@ -162,6 +162,7 @@ void Application::render(){
   SDL_GetWindowSizeInPixels(window_,&w,&h);
   gpu->beginFrame(camera_);
   chunkRenderer_.renderGpuWorld(*gpu,camera_,w,h);
+  gpu->drawHud();
   gpu->endFrame();
   return;
  }
