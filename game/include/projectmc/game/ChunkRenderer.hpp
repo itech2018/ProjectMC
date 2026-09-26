c9f6716a7439b569e590737d72a2fc570dbe51d1
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-26 16:49:59 +0100

    Give chunk renderer world context for face culling

--- game/include/projectmc/game/ChunkRenderer.hpp
+++ game/include/projectmc/game/ChunkRenderer.hpp
@@ -3,13 +3,14 @@
 #include "projectmc/world/Chunk.hpp"
 #include "projectmc/world/ChunkPosition.hpp"
 #include "projectmc/world/BlockRegistry.hpp"
+#include "projectmc/world/World.hpp"
 #include "projectmc/game/Camera.hpp"
 #include "projectmc/game/TextureAtlas.hpp"
 namespace projectmc::game {
 class ChunkRenderer {
 public:
  struct Point{float x{},y{};bool valid{false};};
- void render(SDL_Renderer*,const world::Chunk&,const world::ChunkPosition&,const world::BlockRegistry&,const TextureAtlas&,const Camera&,int,int);
+ void render(SDL_Renderer*,const world::Chunk&,const world::ChunkPosition&,const world::World&,const TextureAtlas&,const Camera&,int,int);
  void renderSelection(SDL_Renderer*,int,int,int,const Camera&,int,int);
  [[nodiscard]] Point project(float,float,float,const Camera&,int,int)const;
 };
