c05c1d954f5537ad5e0d849621e37d879d0fd466
itech2018 <37976121+itech2018@users.noreply.github.com>
2026-09-26 16:51:40 +0100

    Expose world-level voxel render pass

--- game/include/projectmc/game/ChunkRenderer.hpp
+++ game/include/projectmc/game/ChunkRenderer.hpp
@@ -10,7 +10,7 @@ namespace projectmc::game {
 class ChunkRenderer {
 public:
  struct Point{float x{},y{};bool valid{false};};
- void render(SDL_Renderer*,const world::Chunk&,const world::ChunkPosition&,const world::World&,const TextureAtlas&,const Camera&,int,int);
+ void renderWorld(SDL_Renderer*,const world::World&,const TextureAtlas&,const Camera&,int,int);
  void renderSelection(SDL_Renderer*,int,int,int,const Camera&,int,int);
  [[nodiscard]] Point project(float,float,float,const Camera&,int,int)const;
 };
